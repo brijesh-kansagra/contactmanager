d12566dcb31aa3bfa3dd8108686daec8de7ead51
Brijesh Kansagra <bkansagra@mobiquityinc.com>
2020-05-14 15:11:50 +0530

    Updates

---
+++ precache-manifest.e83639584e25e1d99eb9b2616edb7272.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6654c4236e418113f2b5fb487a0702",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1b0da5783905bc4a1954",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8f45beefbdc3ebef16e6",
+    "url": "/contactmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1b0da5783905bc4a1954",
+    "url": "/contactmanager/static/js/2.8bc1fdf7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.8bc1fdf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f45beefbdc3ebef16e6",
+    "url": "/contactmanager/static/js/main.866a3061.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
